e62ec520aa0c195bff0debdbd5b18010dca24332
orion <munna.csehstu@gmail.com>
2024-10-18 12:08:55 +0600

    added UserCard component

--- core-concepts-part-1/src/App.jsx
+++ core-concepts-part-1/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import UserCard from './assets/components/UserCard'
 function App() {
-
-
   return (
     <>
-    <h1>Hello my name is Maruf</h1>
+    <h1>From App</h1>
+    <div className="card-container">
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    <UserCard></UserCard>
+    </div>
+   
     </>
   )
 }
---
+++ core-concepts-part-1/src/assets/components/UserCard.jsx
@@ -0,0 +1,15 @@
+import "./UserCard.css"
+const UserCard = () => {
+    return (
+        // card component
+        <div className="card">
+            <div className="img-container">
+                <img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?t=st=1729230425~exp=1729234025~hmac=9d4a6198dd1560dd3f4ad324467388269d159e92f4bf53e9625502c0998369bd&w=996"></img>
+            </div>
+            <h3>Andre Russell</h3>
+            <p>Front End Developer</p>
+        </div>
+    )
+}
+
+export default UserCard;
