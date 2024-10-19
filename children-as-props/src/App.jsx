aad66a78c4750f138dd11ba9b4398a3dbd75d6f4
orion <munna.csehstu@gmail.com>
2024-10-19 19:33:30 +0600

    renamed props as children to children as props

--- children-as-props/src/App.jsx
+++ children-as-props/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ChildrenAsProps from './ChildrenAsProps'
 import FunctionAsProps from './FunctionAsProps'
