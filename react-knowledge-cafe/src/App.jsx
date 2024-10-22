9789a9dd8cbc7dc26ec3d6b81959e71c1490d204
orion <munna.csehstu@gmail.com>
2024-10-23 00:11:41 +0600

    added react-knowledge-cafe project and completed setup with tailwind css

---
+++ react-knowledge-cafe/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      {/* tailwind setup test */}
+      <h1 className="text-red-400">Hello</h1>
+    </>
+  )
+}
+
+export default App
