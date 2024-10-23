f84d477614eae60288d71571f6b4f531a718bb9a
orion <munna.csehstu@gmail.com>
2024-10-23 13:22:49 +0600

    installed react icons and completed author section

--- react-knowledge-cafe/src/components/blog/Blog.jsx
+++ react-knowledge-cafe/src/components/blog/Blog.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FaRegBookmark } from "react-icons/fa6";
 
 const Blog = ({ blog }) => {
     const { id, cover_img, title, author, author_img, post_date, reading_time, hashtags } = blog;
-   
+
     return (
         <div>
+            {/* cover image */}
             <div>
                 <img src={cover_img} alt="" />
+            </div>
+            {/* author and bookmark section */}
+            <div>
+
+                <div className="flex justify-between items-center">
+                    {/* author */}
+                    <div className="flex justify-center  items-center gap-4">
+                        <div className="w-14">
+                            <img src={author_img} alt="" />
+                        </div>
+                        <div>
+                            <h3>{author}</h3>
+                            <p>{post_date}</p>
+                        </div>
+                    </div>
+                    {/* bookmark */}
+                    <div className="flex justify-center items-center gap-2">
+                        <p>{reading_time} minute read</p>
+                        <button> <FaRegBookmark /></button>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
